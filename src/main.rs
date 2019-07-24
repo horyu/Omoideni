78dcb1d603d86b7ec20a455e5011186b59fd1486
horyu <horyu@users.noreply.github.com>
2019-07-24 22:24:31 +0900

    引数処理まで実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = std::env::args().collect();
+    args.remove(0); // 最初の要素(=バイナリ) を削除
+    let entries = args.iter().filter(|e| Path::new(e).exists());
+    for entry in entries {
+        println!("{:?}", entry);
+    }
 }
