7f6f4522b4f4df94cf4e1b9eff08ba7b4a9d7632
horyu <horyu@users.noreply.github.com>
2019-07-25 20:49:04 +0900

    実在しない入力の有無をチェック

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::path::Path;
 
 fn main() {
-    let mut args: Vec<String> = std::env::args().collect();
-    args.remove(0); // 最初の要素(=バイナリ) を削除
-    let entries = args.iter().filter(|e| Path::new(e).exists());
-    for entry in entries {
-        println!("{:?}", entry);
+    let mut inputs: Vec<String> = std::env::args().collect();
+    inputs.remove(0); // 最初の要素（=実行ファイル）を削除
+    let mut invalid_inputs: Vec<String> = Vec::new(); 
+    let mut valid_inputs: Vec<String> = Vec::new(); 
+    for input in inputs.iter() {
+        if Path::new(input).exists() {
+            valid_inputs.push(input.to_string());
+        } else {
+            invalid_inputs.push(input.to_string());
+        }
+    }
+    // 存在しないファイルがあれば終了
+    if !invalid_inputs.is_empty() {
+        eprintln!("[Error]");
+        for invalid_input in invalid_inputs {
+            eprintln!("\"{}\" is not exist.", invalid_input);
+        }
+        std::process::exit(-1);
     }
 }
