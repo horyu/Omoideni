845c54af71614f759d203a1754f97dbb70077e7c
horyu <horyu@users.noreply.github.com>
2019-09-21 11:56:55 +0900

    コードのフォーマット　＆　エラーメッセージの追加

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
 fn main() {
     let inputs: Vec<String> = std::env::args().collect();
-    let mut invalid_inputs: Vec<String> = Vec::new(); 
-    let mut valid_inputs: Vec<String> = Vec::new(); 
+    let mut invalid_inputs: Vec<String> = Vec::new();
+    let mut valid_inputs: Vec<String> = Vec::new();
     // 最初の要素（=実行ファイル）は無視
     for input in &inputs[1..] {
         if Path::new(input).exists() {
@@ -28,7 +28,8 @@ fn main() {
 }
 
 fn make_empty(target: &str) {
-    let metadata = fs::metadata(target).unwrap();
+    let metadata = fs::metadata(target)
+        .unwrap_or_else(|_| panic!("\"{}\"'s metadata is unavailable.", target));
     let file_type = metadata.file_type();
     if file_type.is_file() {
         make_file_enpty(target);
@@ -41,12 +42,17 @@ fn make_empty(target: &str) {
 fn make_file_enpty(file: &str) {
     // println!("[file]\t{}", file);
     println!("{}", file);
-    fs::OpenOptions::new().write(true).truncate(true).open(file).unwrap();
+    fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(file)
+        .unwrap_or_else(|_| panic!("\"{}\" is unavailable.", file));
 }
 
 fn make_dir_enpty(dir: &str) {
     // println!("[dir]\t{}", dir);
-    for entry in fs::read_dir(dir).unwrap() {
+    let read_dir = fs::read_dir(dir).unwrap_or_else(|_| panic!("\"{}\" is unavailable", dir));
+    for entry in read_dir {
         let entry_path = entry.unwrap().path();
         let entry_path_as_osstr = entry_path.into_os_string();
         let entry_path_as_str = entry_path_as_osstr.to_str().unwrap();
